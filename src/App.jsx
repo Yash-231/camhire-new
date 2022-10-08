c122223da728d5e26d9bb2b89ef6a594fba9a513
Yash-231 <yashsingalkar@gmail.com>
2022-10-08 14:12:57 +0530

    antd demo

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Gallery from "./components/Gallery";
 import Contact from "./components/Contact";
 import header from "./components/header"
 import camHireImage from './components/img/CAMHIRE.png';
-
+import { Button } from 'antd';
 import { Nav, Container, Navbar } from "react-bootstrap";
 
 import { render } from "react-dom";
@@ -24,6 +24,7 @@ function App() {
   return (
 
     <div className="App">
+      <Button type="primary">Button</Button>
       <>
         <Navbar bg="dark" variant="dark"  className="Navbar">
           <Container >
