af322197a73e282e7036311f0e68a8403f42b09f
Yash-231 <yashsingalkar@gmail.com>
2022-10-20 00:32:04 +0530

    individual blog page

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 function Footer() {
   return (
-      <div style={{ padding: "2%", background: "#475462", textAlign: "center" }}>
+      <div style={{ marginTop: "5rem", padding: "2%", background: "#475462", textAlign: "center" }}>
           <p style={{ margin: 0, fontWeight: 500, fontSize: "0.75rem" }}>Camhire VENDOUT INDIA PRIVATE LIMITED</p>
       </div>
   )
